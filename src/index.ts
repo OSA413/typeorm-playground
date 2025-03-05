f4feb20a1533806c9f3e46c626632bfbf0ec7aa7
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2025-03-05 19:46:15 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ AppDataSource.initialize().then(async dataSource => {
     const repo = dataSource.getRepository(Album);
     await repo.save({numberOfLikes: 4123, albumId: 123, title: "test"});
     console.log(await repo.createQueryBuilder().getOne());
-    console.log(await repo.createQueryBuilder().select(["Album.albumId", "Album.number_of_likes", "Album.total", "Album.title"]).getOne());
+    console.log(await repo.createQueryBuilder().select(["Album.albumId", "Album.numberOfLikes", "Album.total", "Album.title"]).getOne());
 })
